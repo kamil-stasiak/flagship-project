5eeca9a12d1e4f5bb4493f05c3e03824bfbeb0a1
Kamil Stasiak <kamilstasiaknew@gmail.com>
2022-06-10 09:08:08 +0200

    Refactor SQL delight (not final solution)

--- buildSrc/src/main/kotlin/postgres-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/postgres-conventions.gradle.kts
@@ -4,10 +4,16 @@ plugins {
 }
 
 dependencies {
-    // todo extract testContainers version
     val test_containers_version: String by project
-    val postgres_version: String by project
-
     testImplementation("org.testcontainers:postgresql:${test_containers_version}")
+    val postgres_version: String by project
     implementation("org.postgresql:postgresql:${postgres_version}")
+
+    val sqdelight_version: String by project
+    implementation("com.squareup.sqldelight:jdbc-driver:${sqdelight_version}")
+    implementation("com.squareup.sqldelight:sqlite-driver:${sqdelight_version}")
+    implementation("com.squareup.sqldelight:gradle-plugin:${sqdelight_version}")
+
+    val hikari_version: String by project
+    implementation("com.zaxxer:HikariCP:${hikari_version}")
 }
